0876b22e9e2e0141ea499adb6d9ac74bca0bcbfc
GabrielBrotas <gabrielbrotas22@gmail.com>
2020-06-28 16:05:53 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, {useState, useEffect} from 'react';
 
-// config para o consumo da API via axios
 import api from './services/api'
 
-// libraries ------
-
-// lodash é um biblioteca que teem funções como uniqueId, debounce, etc. vamos querer um uniqueId para geerar id unico para os arquivos que fizerem upload
 import {uniqueId} from 'lodash'
  
-// vai fazer as correções de tamanhos ex: bytes-kb/mb...
+
 import filesize from 'filesize' 
 
-// styles ------
+
 import GlobalStyle from './styles/global'
 import {Container, Content} from './styles'
 
-// componentes ------
+
 import Upload from './components/Upload'
 import FileList from './components/FileList'
 
@@ -27,7 +23,6 @@ function App() {
   const [uploadFinish, setUploadFinish] = useState([])
 
   
-  // quando um upload for concluido...
   useEffect( () => {
 
     const uploadedFileId = uploadFinish[0]
@@ -53,11 +48,9 @@ function App() {
       )
     }
 
-    // eslint-disable-next-line
   }, [uploadFinish])
 
 
-  // quando um progresso for finalizado...
   useEffect( () => {
 
     const id = changeProgress[1]
@@ -73,22 +66,20 @@ function App() {
       )
     } 
 
-    // eslint-disable-next-line
   }, [changeProgress])
 
 
-  // durante o processo de upload de cada arquivo...
+
   const processUpload = (uploadedFile) => {
     
-    // FormData é como se estivesse dando submit de um form html atraves do JS, o FormData é o objeto que o html transforma os nossos campos do formulario dentro do JS.
     const data = new FormData();
 
-    // colocar o campo 'file' que foi o nome dado no backend ao enviar um upload, o campo uploadedFile.file que são os dados do arquivo que vao para o backend
+  
     data.append('file', uploadedFile.file, uploadedFile.name)
 
-    // enviar para /posts o formulario 'data' que contem a imagem e pegar o progresso do upload
+    
     api.post('posts', data, {
-      // monitorar o quanto do upload foi processado
+      
       onUploadProgress: e => {
         
         const progress = parseInt(Math.round( (e.loaded * 100) / e.total ))
@@ -113,46 +104,46 @@ function App() {
   
   }
 
-  // ao arrastar um arquivo para upload
+
   const handleUpload = files => {
 
-    //  file => ({}) para retornar um objeto
+  
     const newUploadedFile = files.map( file => ({
       file,
       id: uniqueId(),
       name: file.name,
-      // os arquivos vem em bytes, vamos transformalos em kb-mb-etc
+   
       readableSize: filesize(file.size),
-      preview: URL.createObjectURL(file), // url de preview antes de a imagem chegar no servidor
+      preview: URL.createObjectURL(file),
       progress: 0,
       uploaded: false,
       error: false,
-      url: null, // url que vai redirecionar para a imagem, inicia como null pois só vai ser preenchida depois de preenchido o upload
+      url: null, 
     }))
 
-    //  para nao sobrepor o estado e sempre adicionar mais informações
+   
     setUploadedFiles(uploadedFiles.concat(newUploadedFile));
 
     setNewFile([...newUploadedFile])
     
   }
 
-  // para cada novo arquivo que for fazer upload...
+
   useEffect( () => {
     
     newFiles.forEach( (file) => processUpload(file)) 
     
-    // eslint-disable-next-line
+   
   }, [newFiles])
 
-  // deletar um arquivo que foi feito upload da API
+  
   const handleDelete = async id => {
     await api.delete(`posts/${id}`)
     setUploadedFiles( uploadedFiles.filter( file => file.id !== id));
   }
 
   
-  // renderizar os arquivos que estao no servidor na tela
+
   useEffect( () =>{ 
 
     api.get('posts').then(files => {
@@ -167,11 +158,11 @@ function App() {
         url: file.url
       })))
 
-      // deletar todos os objects url criados anteriormente de o upload ser feito para o preview, deletar para nao pesar o servidor
+      
       uploadedFiles.forEach( file => URL.revokeObjectURL(file.preview))
     })
 
-    // eslint-disable-next-line
+    
   }, [])
 
   return <Container>
@@ -179,6 +170,7 @@ function App() {
     <Content>
 
       <Upload onUpload={handleUpload} />
+      
       
       {!!uploadedFiles.length && (
         <FileList  files={uploadedFiles} onDelete={handleDelete}/>
